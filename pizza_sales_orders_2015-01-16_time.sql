a7ad9b7fa4fcde9344551e0f67a8a45980626f64
brian <otienolubalo@gmail.com>
2025-04-30 12:01:28 +0300

    Retrieve pizza orders on 2015-01-16 between 12:12:38 and 13:00:00

---
+++ pizza_sales_orders_2015-01-16_time.sql
@@ -0,0 +1,17 @@
+SELECT
+    od.order_id,
+    pt.name AS Pizza_name,
+    pt.category,
+    p.size,
+    od.quantity,
+    p.price,
+    o.date,
+    o.time,
+    (od.quantity * p.price) AS total_price,
+    COUNT(od.quantity) OVER() AS Total_Quantity_Sold
+FROM order_details od
+LEFT JOIN Pizza p ON od.pizza_id = p.pizza_id
+LEFT JOIN pizza_types pt ON p.pizza_type_id = pt.pizza_type_id
+LEFT JOIN orders o ON od.order_id = o.order_id
+WHERE o.date = '2015-01-16'
+  AND o.time BETWEEN '12:12:38' AND '13:00:00';
